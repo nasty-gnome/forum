7aa957121929049e5f3b4f784e0f85ddd64ad9c5
Yakov Zabelin <zabelinakov@mail.ru>
2022-04-03 15:00:57 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+import json
+import csv
+from flask import Flask
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/main')
+def main_page():
+    return "Главная страница"
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
