4f331726c380226d933e65f9e5de633dad500f2d
lapa25 <lapantonina2505@mail.ru>
2022-04-05 18:44:41 +0300

    регистрация + вход (2 версия)

--- login.py
+++ login.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, redirect
 from forms.login_form import LoginForm
-from forms.register_form import RegisterForm
 from flask_login import LoginManager, login_user
 from data import db_session
 from data.users import User
+import os
+from forms.register_form import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'key'
@@ -19,20 +20,40 @@ def main():
     def reqister():
         form = RegisterForm()
         if form.validate_on_submit():
-            if form.password.data != form.password_again.data:
-                return render_template('register.html', title='Регистрация',
-                                       form=form,
-                                       message="Пароли не совпадают")
             if db_sess.query(User).filter(User.login == form.login.data).first():
                 return render_template('register.html', title='Регистрация',
                                        form=form,
                                        message="Такой пользователь уже есть")
+            elif form.password.data != form.password_again.data:
+                return render_template('register.html', title='Регистрация',
+                                       form=form,
+                                       message="Пароли не совпадают")
+            elif len(form.password.data) < 8:
+                return render_template('register.html', title='Регистрация',
+                                       form=form,
+                                       message="Короткий пароль")
+            elif form.password.data.isdigit():
+                return render_template('register.html', title='Регистрация',
+                                       form=form,
+                                       message="В пароле нет букв")
+            elif form.password.data.isalpha():
+                return render_template('register.html', title='Регистрация',
+                                       form=form,
+                                       message="В пароле нет цифр")
+            elif form.password.data.lower() == form.password.data:
+                return render_template('register.html', title='Регистрация',
+                                       form=form,
+                                       message="В пароле нет букв разных регистров")
+            f = form.photo.data
+            t = f.read()
             user = User(
-                login=form.login.data
+                login=form.login.data,
+                photo=t
             )
             user.set_password(form.password.data)
             db_sess.add(user)
             db_sess.commit()
+            return redirect('/')
         return render_template('register.html', title='Регистрация', form=form)
 
     @login_manager.user_loader
