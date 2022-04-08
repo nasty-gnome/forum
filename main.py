5046cd682ee779e1f2ad339ca602b56dabd1da11
Yakov Zabelin <zabelinakov@mail.ru>
2022-04-08 22:30:32 +0300

    Главная c кнопками (ведут на тестовую страницу)

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import sys
 import json
 import csv
+import login
+from data import db_session
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, Blueprint
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/main')
+@app.route('/', methods=['GET', 'POST'])
+#@app.route('/main', methods=['GET', 'POST'])
 def main_page():
+    if request.method == "POST":
+        if request.form['button'] == "Профиль":
+            return redirect('/puk')
+        elif request.form['button'] == "Обсуждения":
+            return redirect('/puk')
     return render_template('main_page.html', title="Главная")
 
 
+@app.route('/puk')
+def puk():
+    return "Пук-пук-пук"
+
+
 if __name__ == '__main__':
+    db_session.global_init("db/users.db")
+    db_sess = db_session.create_session()
     app.run(port=8080, host='127.0.0.1')
